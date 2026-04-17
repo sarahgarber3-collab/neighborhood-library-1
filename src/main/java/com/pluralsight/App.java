8e0c5d51ff2114f33c33f15d27122e70ed6106fc
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-17 15:25:30 -0400

    more updates

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,9 +1,11 @@
 package com.pluralsight;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         Book[] books = new Book[20];
 
         books[0] = new Book(1, "978-0743273565", "The Great Gatsby", null, false);
@@ -27,11 +29,36 @@ public class App {
         books[18] = new Book(19, "978-0385490818", "The Brothers Karamazov", null, false);
         books[19] = new Book(20, "978-0316346627", "Dune", null, false);
 
-        for(int i = 0; i < books.length;i++){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the library management tool");
+        System.out.println("\t1) Show Available books");
+        System.out.println("\t2) Show Checked out books");
+        System.out.println("\t0) Exit");
+        System.out.print("Please choose an option: ");
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1 -> showAvailableBooks(books);
+            case 2 -> {showCheckedOutBooks(books);
+            }
+            case 0 -> {
+                System.out.println("Goodbye");
+
+            }
+
+
+        }
+    }
+}
+    public static void showAvailableBooks(Book[] books) {
+    for (int i = 0; i < books.length;i++){
+        if (!books[i].isCheckedOut() {
             System.out.println(books[i]);
         }
     }
-
-
-
 }
+public static void showCheckedOutBooks(Book[] books) {
+    for (int i = 0; i < books.length;i++){
+        if (books[i].isCheckedOut() {
+            System.out.println(books[i]);
